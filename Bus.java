afe060158c6780a47fe45432831ddea813877002
Miguel <miguelmoutela@hotmail.com>
2017-10-30 20:07:42 +0000

    class Bus defined; tests libraries imported

---
+++ Bus.java
@@ -0,0 +1,11 @@
+class Bear{
+  private String destination;
+  private int number;
+  private Passenger[] capacity;
+
+  public Bus(String destination, int number){
+    this.destination = destination;
+    this.number = number;
+    this.capacity = new Passanger[40];
+  }
+}
---
+++ BusTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class BusTest {
+
+  Bus bus;
+  Passenger passenger;
+
+  @Before
+
+  public void before() {
+    this.bus = 
+  }
+}
