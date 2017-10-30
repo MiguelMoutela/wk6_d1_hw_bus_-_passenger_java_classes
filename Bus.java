296d9acaf96fe001dd757be99f2e14058e7e4e01
Miguel <miguelmoutela@hotmail.com>
2017-10-30 22:29:35 +0000

    dest num and passenger tests passing

--- Bus.java
+++ Bus.java
@@ -1,11 +1,27 @@
 class Bus{
   private String destination;
   private int number;
-  private Passenger[] capacity;
+  private Passenger[] passenger;
 
   public Bus(String destination, int number){
     this.destination = destination;
     this.number = number;
-    this.capacity = new Passenger[0];
+    this.passenger = new Passenger[0];
   }
+  public String getDestination(){
+    return this.destination;
+  }
+  public int getNumber(){
+    return this.number;
+  }
+  public int count(){
+    int count = 0;
+    for(Passenger passenger : passenger){
+      if(passenger != null){
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
--- BusTest.java
+++ BusTest.java
@@ -12,4 +12,17 @@ public class BusTest {
     this.bus = new Bus("Ocean Terminal", 25);
     this.passenger = new Passenger();
   }
+
+  @Test
+  public void hasDestinantion(){
+    assertEquals("Ocean Terminal", bus.getDestination());
+  }
+  @Test
+  public void hasNumber(){
+    assertEquals(25, bus.getNumber());
+  }
+  @Test
+  public void count(){
+    assertEquals(0, bus.count());
+  }
 }
